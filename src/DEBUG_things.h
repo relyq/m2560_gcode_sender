286960c3eadb63cff691a33af396031c86946326
relyq <nicoale_mainenti@hotmail.es>
2022-02-24 04:00:00 -0300

    report lcd driver at startup

--- src/DEBUG_things.h
+++ src/DEBUG_things.h
@@ -5,10 +5,15 @@
 #define DEBUG_PRINT(x) Serial.print(x)
 #define DEBUG_PRINTLN(x) Serial.println(x)
 #define DEBUG_PRINTDEC(x) Serial.print(x, DEC)
+#define DEBUG_PRINTHEX(x) Serial.print(x, HEX)
 #define DEBUG_PRINTLNDEC(x) Serial.println(x, DEC)
+#define DEBUG_PRINTLNHEX(x) Serial.println(x, HEX)
 #else
 #define DEBUG_BEGIN(x)
 #define DEBUG_PRINT(x)
-#define DEBUG_PRINTDEC(x)
 #define DEBUG_PRINTLN(x)
+#define DEBUG_PRINTDEC(x)
+#define DEBUG_PRINTHEX(x)
+#define DEBUG_PRINTLNDEC(x)
+#define DEBUG_PRINTLNHEX(x)
 #endif
--- src/tasks/taskTouchscreenMenu.cpp
+++ src/tasks/taskTouchscreenMenu.cpp
@@ -32,14 +32,43 @@ void taskTouchscreenMenu(void* pvParameters) {
 
   DEBUG_PRINTLN(F("Touchscreen task started."));
 
+  uint16_t lcd_id = tft.readID();
+  uint8_t lcd_rotation = 0;
+
+  switch (lcd_id) {
+    case 0x9325: {
+      lcd_rotation = 1;
+      break;
+    }
+    case 0x9341: {
+      lcd_rotation = 3;
+      break;
+    }
+    case 0xC0C0: {  // 0x9341
+      lcd_id = 0x9341;
+      lcd_rotation = 3;
+      break;
+    }
+    default: {
+      DEBUG_PRINT(F("UNRECOGNIZED DRIVER (0x"));
+      DEBUG_PRINTHEX(lcd_id);
+      DEBUG_PRINTLN(F(") USING 0x9341 DRIVER"));
+      lcd_id = 0x9341;
+      lcd_rotation = 3;
+      break;
+    }
+  }
+
+  DEBUG_PRINT(F("LCD driver reported by screen: 0x"));
+  DEBUG_PRINTLNHEX(lcd_id);
   DEBUG_PRINT(F("TFT size is "));
   DEBUG_PRINT(tft.width());
   DEBUG_PRINT(F("x"));
   DEBUG_PRINTLN(tft.height());
 
   tft.reset();
-  tft.begin(0x9341);
-  tft.setRotation(3);
+  tft.begin(lcd_id);
+  tft.setRotation(lcd_rotation);
 
   tft.fillScreen(BLACK);
 
