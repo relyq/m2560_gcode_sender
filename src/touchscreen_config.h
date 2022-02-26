d0061a9f4bc240de0105b35a5162ad1a5dc3cf52
relyq <nicoale_mainenti@hotmail.es>
2022-02-26 18:39:04 -0300

    added support for new touchscreen

--- src/touchscreen_config.h
+++ src/touchscreen_config.h
@@ -12,18 +12,35 @@
 #define LCD_RESET A4  // Can alternately just connect to Arduino's reset pin
 
 // touchscreen
+#define TS0 0
+#define TS1 1
+#if TS0
 #define YP A3   // must be an analog pin // LCD CS
 #define XM A2   // must be an analog pin // LCD RS - COMMAND/DATA
 #define YM 9    // can be a digital pin  // LCD D1
 #define XP 8    // can be a digital pin  // LCD D0
 #define RX 350  // resistance between x+ and x-
+#elif TS1
+#define YP A1   // must be an analog pin // LCD WR
+#define XM A2   // must be an analog pin // LCD RS - COMMAND/DATA
+#define YM 7    // can be a digital pin  // LCD D1
+#define XP 6    // can be a digital pin  // LCD D6
+#define RX 615  // resistance between x+ and x-
+#endif
 
 // puntos maximos y minimos de la pantalla tactil, contando el espacio
 // no-dibujable
+#if TS0
 #define TS_MINX 120
 #define TS_MINY 75
 #define TS_MAXX 900
 #define TS_MAXY 950
+#elif TS1
+#define TS_MINX 150
+#define TS_MINY 950
+#define TS_MAXX 920
+#define TS_MAXY 200
+#endif
 
 #define MINPRESSURE 10
 #define MAXPRESSURE 1000
