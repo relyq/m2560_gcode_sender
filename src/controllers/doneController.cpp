f97c2b01ec74d86acc08c2d782a1096579284305
relyq <relyqx@gmail.com>
2023-03-19 16:35:24 -0300

    dont move when done

--- src/controllers/doneController.cpp
+++ src/controllers/doneController.cpp
@@ -9,7 +9,7 @@ extern uint8_t currentFile;
 extern char** files;
 
 void doneController(Adafruit_GFX* tft, Adafruit_GFX_Button* buttonsHome) {
-  xQueueSend(qGcodeLine, "G53G0X-150Y-300Z-1\n", portMAX_DELAY);
+  // xQueueSend(qGcodeLine, "G53G0X-150Y-300Z-1\n", portMAX_DELAY);
   currentScreen = Screens::Home;
   drawHomeScreen(tft, buttonsHome, files[currentFile]);
 }
